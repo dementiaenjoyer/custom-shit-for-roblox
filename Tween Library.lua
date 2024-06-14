ecf87c2eda8476aa41c5e37ba8bae8b40a366015
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-06-14 18:37:12 +0000

    Update Tween Library.lua

--- Tween Library.lua
+++ Tween Library.lua
@@ -2,49 +2,50 @@
 local TweenLibrary = {}
 local Other = {}
 TweenLibrary.__index = TweenLibrary
---// Rest
- 
-do --// Functions
 
-    do --// Components
+--// Functions
 
-        function Other.Lerp(Start, End, Interp)
-            return Start + (End - Start) * Interp
-        end
+do --// Components
 
-        function Other.LerpColor(Color1, Color2, Interp)
-            return {
-                R = Other.Lerp(Color1.r, Color2.r, Interp),
-                G = Other.Lerp(Color1.g, Color2.g, Interp),
-                B = Other.Lerp(Color1.b, Color2.b, Interp)
-            }
-        end
-
-        function Other.LerpTransparency(Transparency1, Transparency2, Interp)
-            return Other.Lerp(Transparency1, Transparency2, Interp)
-        end
-
+    function Other.Lerp(Start, End, Interp)
+        return Start + (End - Start) * Interp
     end
 
-    function TweenLibrary:Tween(Object, Property, EndValue, Duration)
-        local StartValue = Object[Property]
-        local StartTime = tick()
-
-        local function Update()
-            local Progressed = tick() - StartTime
-            local Progress = math.min(Progressed / Duration, 1)
-            if Property:find("Color") then
-                Object[Property] = Other.LerpColor(StartValue, EndValue, Progress)
-            elseif Property:find("Transparency") then
-                Object[Property] = Other.LerpTransparency(StartValue, EndValue, Progress)
-            end
-            if Progress < 1 then
-                task.wait()
-                Update()
-            end
-        end
-        Update()
+    function Other.LerpColor(Color1, Color2, Interp)
+        return Color3.new(
+            Other.Lerp(Color1.R, Color2.R, Interp),
+            Other.Lerp(Color1.G, Color2.G, Interp),
+            Other.Lerp(Color1.B, Color2.B, Interp)
+        )
     end
+
+    function Other.LerpTransparency(Transparency1, Transparency2, Interp)
+        return Other.Lerp(Transparency1, Transparency2, Interp)
+    end
+
+end
+
+function TweenLibrary:Tween(Object, Property, EndValue, Duration)
+    local StartValue = Object[Property]
+    local StartTime = tick()
+
+    local function Update()
+        local ElapsedTime = tick() - StartTime
+        local Progress = math.min(ElapsedTime / Duration, 1)
+
+        if Property:find("Color") then
+            Object[Property] = Other.LerpColor(StartValue, EndValue, Progress)
+        elseif Property:find("Transparency") then
+            Object[Property] = Other.LerpTransparency(StartValue, EndValue, Progress)
+        end
+
+        if Progress < 1 then
+            task.wait()
+            Update()
+        end
+    end
+
+    Update()
 end
 
 return TweenLibrary
