7eda613cc13791a57cbabae9349892264911fd0b
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-02-21 15:58:43 +0100

    Update BoxHandleAdornmentChams.lua

--- BoxHandleAdornmentChams.lua
+++ BoxHandleAdornmentChams.lua
@@ -1,20 +1,48 @@
+-- This code is pretty bad, but i just wanted to get it working so i did some ghetto stuff
+
 local run_service = game.GetService(game, "RunService");
-local chams_parent = (gethui and gethui()) or (cloneref and cloneref(game.GetService(game, "CoreGui"))) or game.GetService(game, "CoreGui");
+local chams_parent = run_service:IsStudio() and workspace or ((gethui and gethui()) or (cloneref and cloneref(game.GetService(game, "CoreGui"))) or game.GetService(game, "CoreGui"));
 
 local chams = {}; do
 	-- Utility
+	local sizes = {
+		["Head"] = Vector3.new(1, 1, 1),
+		["Torso"] = Vector3.new(2, 2, 1),
+		["Right Leg"] = Vector3.new(1, 2, 1),
+		["Left Leg"] = Vector3.new(1, 2, 1),
+		["Right Arm"] = Vector3.new(1, 2, 1),
+		["Left Arm"] = Vector3.new(1, 2, 1),
+	};
+	
+	local mesh_ids = {
+		["rbxassetid://6179256256"] = "Head",
+		["rbxassetid://4049240078"] = "Torso",
+		["rbxassetid://4049240323"] = "Right Leg",
+		["rbxassetid://4049240209"] = "Left Leg",
+	};
+	
+	local get_bodypart = function(bodypart, size, mesh)
+		local bodypart_name = bodypart.Name;
+		local bodypart_size_name = (function()
+			for name, s in sizes do
+				if (s == size) then
+					return name;
+				end
+			end
+		end)
+		
+		return (sizes[bodypart_name] and bodypart_name) or (bodypart_size_name) or (mesh_ids[mesh])
+	end
+	
 	chams.connection = nil;
 	chams.created = {};
-	chams.utility = {
-		["is_head"] = function(bodypart, size, mesh)
-			return (bodypart.Name == "Head") or (size == Vector3.new(1, 1, 1)) or (mesh == "rbxassetid://6179256256");
-		end,
-	};
 
 	-- Settings
 	chams.enabled = true;
+	chams.fetch_sizes = true; -- R6 ONLY
 	chams.occluded_color = Color3.fromRGB(58, 61, 255);
 	chams.visible_color = Color3.fromRGB(255, 255, 255);
+	chams.parent = chams_parent;
 
 	-- Functions
 	chams.update = function(cham_object, character)
@@ -50,29 +78,34 @@ local chams = {}; do
 
 				if (bodypart_name ~= "HumanoidRootPart") then
 					local mesh_part = bodypart.FindFirstChildOfClass(bodypart, "SpecialMesh");
-					local identify_head = chams.utility.is_head(bodypart, bodypart.Size, (mesh_part and mesh_part.MeshId) or "")
-
+					local mesh_id = (mesh_part and mesh_part.MeshId) or "";
+					local fetched_bodypart = get_bodypart(bodypart, Vector3.zero, mesh_id);
+					
 					if (not chams.created[character][bodypart]) then
 						chams.created[character][bodypart] = {};
 					end
 
 					local cached_bodyparts = chams.created[character][bodypart];
-					local occluded = Instance.new("BoxHandleAdornment", chams_parent);
+					local occluded = Instance.new("BoxHandleAdornment", chams.parent);
 					local visible = Instance.new("BoxHandleAdornment", occluded);
+					
+					local fetch_sizes = chams.fetch_sizes;
+					local fetch_size = fetch_sizes and sizes[fetched_bodypart];
+					local bodypart_size = bodypart.Size;
 
 					occluded.Adornee = bodypart;
 					occluded.AlwaysOnTop = true;
 					occluded.Transparency = 0.8;
 					occluded.Color3 = chams.occluded_color;
 					occluded.ZIndex = 1;
-					occluded.Size = bodypart.Size;
+					occluded.Size = fetch_size or bodypart_size;
 
 					visible.Adornee = bodypart;
 					visible.Transparency = 0.5;
 					visible.Color3 = chams.visible_color;
 					visible.ZIndex = 1;
-					visible.Size = (identify_head and bodypart.Size * 1.05) or bodypart.Size * 1.03;
 					visible.Parent = occluded;
+					visible.Size = fetch_size or bodypart_size;
 
 					local accessories = character.FindFirstChildOfClass(character, "Folder");
 
@@ -113,7 +146,6 @@ local chams = {}; do
 		chams.created[character][bodypart] = nil;
 	end
 
-
 	chams.unload = function()
 		chams.connection = nil;
 
@@ -123,6 +155,9 @@ local chams = {}; do
 			end
 		end
 	end
+
+
+	chams.create(workspace.Phantoms);
 
 	-- Loops
 	do
